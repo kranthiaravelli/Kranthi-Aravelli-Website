d84f3f55754b2b453589ccbd6218eb80061e7f97
Kranthi Aravelli <aravellykranthi@gmail.com>
2026-07-15 16:27:30 +0530

    Refactor script.js and add toggleText function

--- script.js
+++ script.js
@@ -1,114 +1,133 @@
+console.log("Script Loaded");
+
 const small_menu = document.getElementById('yt_menu_btn');
 const menu_reveal = document.getElementById('yt_menu');
 const menu = document.getElementById('menu');
 const navElements = document.getElementById('nav_elements');
 
-function moreOpen() {
-    if(menu_reveal.style.display === 'flex'){
-        menu_reveal.style.display = 'none';
-    } else {
-        menu_reveal.style.display = 'flex';
-    }
+// function moreOpen() {
+//     if(menu_reveal.style.display === 'flex'){
+//         menu_reveal.style.display = 'none';
+//     } else {
+//         menu_reveal.style.display = 'flex';
+//     }
     
-}
+// }
 
-let song_name;
-let song_type;
-let song_artist;
+// let song_name;
+// let song_type;
+// let song_artist;
 
-const audio = document.getElementById('audio');
-const playBtn = document.getElementById('play');
-const progress = document.getElementById('progress');
+// const audio = document.getElementById('audio');
+// const playBtn = document.getElementById('play');
+// const progress = document.getElementById('progress');
 
-const current = document.getElementById('current');
-const duration = document.getElementById('duration');
+// const current = document.getElementById('current');
+// const duration = document.getElementById('duration');
 
-let playing = false;
+// let playing = false;
 
-// play & pause
+// // play & pause
 
-playBtn.onclick = () => {
-    if(playing){
-        audio.pause();
-        playBtn.innerHTML = '▶';
+// // playBtn.onclick = () => {
+// //     if(playing){
+// //         audio.pause();
+// //         playBtn.innerHTML = '▶';
+// //     } else {
+// //         audio.play();
+// //         playBtn.innerHTML = '❚❚'
+// //     }
+
+// //     playing = !playing;
+// // }
+
+// //Duration
+// // audio.addEventListener('loadedmetadata', ()=>{
+// //     progress.max = Math.floor(audio.duration);
+
+// //     duration.innerHTML = format(audio.duration);
+// // });
+
+// // update progress
+// // audio.addEventListener("timeupdate", ()=> {
+// //     progress.value = Math.floor(audio.currentTime);
+
+// //     current.innerHTML = format(audio.currentTime);
+// // });
+
+// // Seek
+
+// progress.oninput = ()=> {
+//     audio.currentTime = progress.value;
+// };
+
+// // Song End
+// audio.onended = ()=> {
+//     playing = false;
+
+//     playBtn.innerHTML = '▶';
+// };
+
+// // Format Time
+// function format(time){
+//     let min = Math.floor(time/60);
+
+//     let sec = Math.floor(time%60);
+
+//     if (sec<10) sec = '0'+sec;
+
+//     return min+":"+sec
+// }
+
+// audio.addEventListener('timeupdate', ()=> {
+//     progress.value = audio.currentTime;
+
+//     const percent = (audio.currentTime / audio.duration) * 100;
+
+//     progress.style.background =
+//         `linear-gradient(to right,
+//         #22c55e 0%,
+//         #22c55e ${percent}%,
+//         #444444c4 ${percent}%,
+//         #444 100%)`;
+// });
+
+
+// const song_Title = document.getElementById("song_Title");
+// const song_Artist = document.getElementById("song_Artist");
+
+// function loadSong(title, artist, src) {
+
+//     audio.src = src;
+//     audio.load();
+
+//     song_Title.textContent = title;
+//     song_Artist.textContent = artist;
+
+//     progress.value = 0;
+//     current.textContent = "0:00";
+
+//     audio.play();
+
+//     playing = true;
+//     playBtn.innerHTML = "❚❚";
+// }
+
+const dots = document.getElementById('dots');
+const moreText = document.getElementById('moreText');
+const btnText = document.getElementById('btnText');
+
+function toggleText() {
+
+    if (dots.style.display === "none"){
+        dots.style.display = 'inline';
+        btnText.innerHTML = 'Read More';
+        moreText.style.display = 'none';
     } else {
-        audio.play();
-        playBtn.innerHTML = '❚❚'
+        dots.style.display = 'none';
+        btnText.innerHTML = 'Show less';
+        moreText.style.display = 'inline';
     }
-
-    playing = !playing;
-}
-
-//Duration
-audio.addEventListener('loadedmetadata', ()=>{
-    progress.max = Math.floor(audio.duration);
-
-    duration.innerHTML = format(audio.duration);
-});
-
-// update progress
-audio.addEventListener("timeupdate", ()=> {
-    progress.value = Math.floor(audio.currentTime);
-
-    current.innerHTML = format(audio.currentTime);
-});
-
-// Seek
-
-progress.oninput = ()=> {
-    audio.currentTime = progress.value;
-};
-
-// Song End
-audio.onended = ()=> {
-    playing = false;
-
-    playBtn.innerHTML = '▶';
-};
-
-// Format Time
-function format(time){
-    let min = Math.floor(time/60);
-
-    let sec = Math.floor(time%60);
-
-    if (sec<10) sec = '0'+sec;
-
-    return min+":"+sec
-}
-
-audio.addEventListener('timeupdate', ()=> {
-    progress.value = audio.currentTime;
-
-    const percent = (audio.currentTime / audio.duration) * 100;
-
-    progress.style.background =
-        `linear-gradient(to right,
-        #22c55e 0%,
-        #22c55e ${percent}%,
-        #444444c4 ${percent}%,
-        #444 100%)`;
-});
-
-
-const song_Title = document.getElementById("song_Title");
-const song_Artist = document.getElementById("song_Artist");
-
-function loadSong(title, artist, src) {
-
-    audio.src = src;
-    audio.load();
-
-    song_Title.textContent = title;
-    song_Artist.textContent = artist;
-
-    progress.value = 0;
-    current.textContent = "0:00";
-
-    audio.play();
-
-    playing = true;
-    playBtn.innerHTML = "❚❚";
 }
 
 // function loadSong(title, artist, src) {
@@ -212,3 +231,4 @@ trackCards.forEach(card => {
 //         );
 //     });
 // });
+
